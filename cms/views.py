864d753e4a05cee155700a0b402511e564373d77
José Moreno Rodríguez <35952698+JoseMoreno95@users.noreply.github.com>
2018-04-04 23:56:27 +0200

    Version 1.0

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Pages
 
 # Create your views here.
+
+def mainPage(request):
+    group = Pages.objects.all()
+    list = ''
+    for item in group:
+        list = list + item.name + '<br>'
+    return HttpResponse("La lista de recursos disponibles es la siguiente:<br><br>" +
+    list + '<br>'
+    "Un ejemplo de cómo pedir un recurso es: "
+    "http://localhost:8000/pepito<br><br>"
+    "Si pides un recurso que no existe, la página te lo indicará")
+
+def getPage(request, text):
+    try:
+        object = Pages.objects.get(name = text)
+        return HttpResponse(object.page)
+    except Pages.DoesNotExist:
+        return HttpResponse("No hay una página para " + text)
